6692af44333af22c57b53c3dec1c117e049e2ed9
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-11-04 01:20:06 +0700

    v2
    button

--- Homepage.java
+++ Homepage.java
@@ -1,1 +1,18 @@
-//kuy//
+import javax.swing.*;
+public class Homepage extends JPanel {
+    JButton startbutton = new JButton("Start");
+    JButton settingbutton = new JButton("Setting");
+    JButton exitbutton = new JButton("Exit");
+    Homepage(){
+        add(this);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("The Last Refuge");
+        Homepage home = new Homepage();
+        frame.add(home);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
