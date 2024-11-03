146db11bce412a642eddda27b018f15e7e97a092
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-11-04 01:28:38 +0700

    v1.1
    buttom

--- Homepage.java
+++ Homepage.java
@@ -1,10 +1,24 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.*;
 public class Homepage extends JPanel {
     JButton startbutton = new JButton("Start");
     JButton settingbutton = new JButton("Setting");
     JButton exitbutton = new JButton("Exit");
     Homepage(){
-        add(this);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gid = new GridBagConstraints();
+        gid.gridx = 0;
+        gid.gridy = GridBagConstraints.RELATIVE;
+        gid.insets = new Insets(10, 0, 10, 0);
+        gid.anchor = GridBagConstraints.CENTER;
+        gid.fill = GridBagConstraints.HORIZONTAL;
+
+
+        add(startbutton,gid);
+        add(settingbutton,gid);
+        add(exitbutton,gid);
     }
 
     public static void main(String[] args) {
