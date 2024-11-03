d2e3297993a79c92e188a749be9b6da2dbddf681
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-11-04 01:57:33 +0700

    v1.1..1
    jj

--- Homepage.java
+++ Homepage.java
@@ -1,12 +1,37 @@
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
+
 public class Homepage extends JPanel {
-    JButton startbutton = new JButton("Start");
+    JButton startbutton;
     JButton settingbutton = new JButton("Setting");
     JButton exitbutton = new JButton("Exit");
-    Homepage(){
+    private Image backgroundImage;
+
+    Homepage() {
+    
+        ImageIcon startIcon = new ImageIcon("Start button.png");
+        ImageIcon bghomeIcon = new ImageIcon("bgghome.gif");
+        backgroundImage = bghomeIcon.getImage();
+        
+
+        startbutton = new JButton(startIcon);
+        startbutton.setBorderPainted(false);
+        startbutton.setContentAreaFilled(false); 
+
+       
+        startbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showNextScreen();
+            }
+        });
+
         setLayout(new GridBagLayout());
         GridBagConstraints gid = new GridBagConstraints();
         gid.gridx = 0;
@@ -15,10 +40,21 @@ public class Homepage extends JPanel {
         gid.anchor = GridBagConstraints.CENTER;
         gid.fill = GridBagConstraints.HORIZONTAL;
 
+      
+        add(startbutton, gid);
+        add(settingbutton, gid);
+        add(exitbutton, gid);
+        
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
 
-        add(startbutton,gid);
-        add(settingbutton,gid);
-        add(exitbutton,gid);
+    private void showNextScreen() {
+        JOptionPane.showMessageDialog(this, "Moving  screen...");
     }
 
     public static void main(String[] args) {
@@ -27,6 +63,7 @@ public class Homepage extends JPanel {
         frame.add(home);
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
